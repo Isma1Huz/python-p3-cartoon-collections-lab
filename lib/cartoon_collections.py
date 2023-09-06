70f5140150cfb209f811ba8b66c22abb00a62f2f
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-09-06 09:14:27 +0300

    Done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+# Function to roll call the dwarves
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+# Function to summon Captain Planet
+def summon_captain_planet(planeteer_calls):
+    # Use list comprehension to capitalize and add an exclamation point
+    return [call.capitalize() + '!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+# Function to check if any calls are longer than four characters
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+# Function to find the first cheese in a list of ingredients
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
